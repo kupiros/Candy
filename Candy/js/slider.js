c1a64b39ba5f7dbc2aa00037da0cd6dce3f0049d
Владислав Куприянов <kupiros@gmail.com>
2020-05-07 03:27:38 +0700

    теперь точно все вопросы со слайдером решены

--- Candy/js/slider.js
+++ Candy/js/slider.js
@@ -1,22 +1,25 @@
 new Swiper ('.slider', { //Класс контейнера
+  slideClass: '_sS', //НЕ ТРОГАТЬ
+  wrapperClass: '_sW', //НЕ ТРОГАТЬ
   navigation: { //Классы кнопок
     nextEl: '.slider__btn',
   },
-  slideClass: '_sS', //НЕ ТРОГАТЬ
-  wrapperClass: '_sW', //НЕ ТРОГАТЬ
-  loop: true, //по кругу
+  autoplay: {delay: 5000},//автоплей в МС
+  roundLengths: true, //округление размеров (для норм шрифта)
+  loop: true, //по кругу ли
+  speed: 1500,//в мс
+  allowTouchMove: false,//отключить перетаскивание
   //spaceBetween: 30, //расстояние между слайдами В ПИКСЕЛЯХ
   //slidesPerView: 3, //кол-во отображаемых слайдов
   //grabCursor: true, //заменить курсор на захват
   //centeredSlides: true, //активный слайд в центре
-  speed: 1000,
-  autoHeight: true,
+  //autoHeight: true,
   //slidesPerGroup: 1,
-  //direction: 'vertical',
+  direction: 'vertical',
   /*breakpoints: {
     768: { //начиная с планшета
       slidesPerView: 3,
       spaceBetween: 30
     }, 
   },*/
-})
+});
